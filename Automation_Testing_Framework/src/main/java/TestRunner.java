04accb48522ab2d8d03ff30ac92c2ff2c5a94bac
IgnatencoVlad <ignatencovald@gmail.com>
2023-03-21 13:40:14 +0200

    Homework

--- Automation_Testing_Framework/src/main/java/TestRunner.java
+++ Automation_Testing_Framework/src/main/java/TestRunner.java
@@ -1,2 +1,9 @@
+import Managers.WebDriverManager;
+
 public class TestRunner {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager webDriverManager = new WebDriverManager("FIREFOX");
+        webDriverManager.getDriver().get("https://www.autodoc.de/");
+        webDriverManager.getDriver().close();
+    }
 }
